3dd06046609cdd9935bcd00453bd50a2f08c912a
Nuremir1234567 <163406890+Nuremir1234567@users.noreply.github.com>
2024-03-14 16:35:58 +0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,40 @@
+const menu_listArray = [
+  {
+    id: 1,
+    name: "Burger",
+    price: 50,
+    img: "./img/1.png",
+  },
+  {
+    id: 2,
+    name: "Coffe ",
+    price: 450,
+    img: "./img/1.png",
+  },
+  {
+    id: 3,
+    name: "Coca-Cola",
+    price: 78,
+    img: "./img/2.png",
+  },
+  {
+    id: 4,
+    name: "Pizza",
+    price: 10,
+    img: "./img/1.png",
+  },
+  {
+    id: 5,
+    name: "Fries",
+    price: 40,
+    img: "./img/1.png",
+  },
+  {
+    id: 6,
+    name: "Tea",
+    price: 70,
+    img: "./img/1.png",
+  },
+];
+
+const orders_basked = [];
